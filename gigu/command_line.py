022169d9e4044b1babb65432e277234f8ddee765
Matthias Osswald <m@osswald.li>
2018-12-20 21:19:54 +0100

    Get open pull request data

--- gigu/command_line.py
+++ gigu/command_line.py
@@ -1,13 +1,37 @@
 from dotenv import load_dotenv
 import os
+from github3 import login
 
 BASE_PATH = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 load_dotenv(os.path.join(BASE_PATH, '.env'))
 
 github_token = os.getenv('GITHUB_TOKEN')
 
+PULL_INFO = []
+
+
 def main():
-    pass
+    gh = login(token=github_token)
+    ftw = gh.organization('4teamwork')
+    repos = ftw.repositories()
+    for repo in repos:
+        for pull in repo.pull_requests():
+            if pull.state != 'open':
+                continue
+
+            last_updated = pull.updated_at or pull.created_at
+
+            PULL_INFO.append({
+                'pull_title': pull.title,
+                'last_updated': last_updated,
+                'url': pull.html_url,
+                'creator': pull.user.login,
+                'assignees': [assignee.login for assignee in
+                              pull.assignees],
+                'reviewers': [reviewer.login for reviewer in
+                              pull.requested_reviewers],
+            })
+    print(PULL_INFO)
 
 
 if __name__ == '__main__':
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ DESCRIPTION = 'Pings into pull requests which are not reviewed/proceeding.'
 URL = 'https://github.com/4teamwork/ftw.gigu'
 REQUIRED = [
     'python-dotenv==0.9.1',
-    'PyGithub==1.43.3',
+    'github3.py==1.2.0',
 ]
 
 setuptools.setup(
