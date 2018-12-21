59767a2f0d3e09553c2745018dbd93b9de6b2fbc
Matthias Osswald <m@osswald.li>
2018-12-21 10:33:14 +0100

    Add data analysis

--- gigu/command_line.py
+++ gigu/command_line.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
-import os
 from gigu.data_handling import DataHandling
+from gigu.plotting_pull_data import PlottingTools
+import os
 
 BASE_PATH = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 load_dotenv(os.path.join(BASE_PATH, '.env'))
@@ -10,11 +11,14 @@ organisation = '4teamwork'
 
 
 def main():
-    instance = DataHandling(organisation, github_token)
-    instance.collect_open_pull_request_data()
-    instance.write_pull_info_to_file()
-    # data = instance.open_from_file('pull_info_2018_12_20_223942.json')
-    # print(data)
+    data_instance = DataHandling(organisation, github_token)
+    data_instance.collect_open_pull_request_data()
+    file_path = data_instance.write_pull_info_to_file()
+
+    plot_instance = PlottingTools(file_path)
+    plot_instance.graph_pulls_per_week()
+    plot_instance.average_time_pulls_open_per_user()
+    plot_instance.open_pulls_for_username()
 
 
 if __name__ == '__main__':
--- gigu/plotting_pull_data.py
+++ gigu/plotting_pull_data.py
@@ -1,16 +1,28 @@
 from gigu.data_handling import DataHandling
-import time
+import json
+import matplotlib as mpl
 import matplotlib.pyplot as plt
+import time
 
 
 class PlottingTools:
-    def __init__(self):
-        instance = DataHandling('empty', 'empty')
-        self.pull_data = instance.open_from_file(
-            'pull_info_2018_12_21_012300.json')
+    def __init__(self, file_path):
+        self.pull_data = self.open_from_file(file_path)
+        self.params = [el for el in mpl.rcParams.keys() if 'size' in el]
+
+    def open_from_file(self, file_path):
+        with open(file_path, 'r') as infile:
+            data = json.load(infile)
+        return data
+
+    def get_creator_list(self):
+        creators = list(set(list(map(lambda d: d['creator'], self.pull_data))))
+        return creators
 
     def graph_pulls_per_week(self):
-        timestamps = [x['last_updated'] for x in self.pull_data]
+        pull_data_open = [x for x in self.pull_data
+                          if x['pull_state'] == 'open']
+        timestamps = [x['last_updated'] for x in pull_data_open]
         earliest = min(timestamps)
 
         ts = earliest
@@ -18,23 +30,51 @@ class PlottingTools:
         list_x = []
         list_y = []
         while ts <= time.time():
-            y = [x for x in self.pull_data
+            y = [x for x in pull_data_open
                  if ts <= x['last_updated'] < (ts + 604800)]
             list_x.append(i)
             list_y.append(len(y))
             i -= 1
             ts += 604800
 
-        plt.plot(list_x[::-1], list_y)
-        plt.xlabel('Weeks in past')
+        plt.figure(figsize=(10, 10))
+        plt.bar(list_x[::-1], list_y)
+        plt.title('Number of pulls in week now - x')
+        plt.xlabel('x\'th week (0 is today)')
         plt.ylabel('unclosed pulls')
         plt.show()
 
-    def output_sorted_by_date(self):
-        # data_sorted = sorted(self.pull_data, key=lambda k: k['last_updated'])
-        return self.pull_data
+    def average_time_pulls_open_per_user(self):
+        x = self.get_creator_list()
+        y = []
+        for user in x:
+            pulls_of_user = [p for p in self.pull_data
+                             if p['creator'] == user]
+            amount_pulls = len(pulls_of_user)
+            time_from_open_to_close = [p['merged'] - p['created']
+                                       for p in pulls_of_user
+                                       if p['merged']]
+            y.append(sum(time_from_open_to_close) / amount_pulls)
 
-instance = PlottingTools()
-# instance.graph_pulls_per_week()
-print(instance.output_sorted_by_date())
-print(len(instance.output_sorted_by_date()))
+        plt.figure(figsize=(10, 14))
+        plt.barh(x, y)
+        plt.title('Average Time for pulls to merge per user')
+        plt.xlabel('Username')
+        plt.ylabel('Average time for pulls to merge')
+        plt.show()
+
+    def open_pulls_for_username(self):
+        x = self.get_creator_list()
+        y = []
+        for user in x:
+            u_pulls = [x for x in self.pull_data if x['creator'] == user
+                       and x['pull_state'] == 'open']
+            open_pulls = len(u_pulls)
+            y.append(open_pulls)
+
+        plt.figure(figsize=(10, 14))
+        plt.barh(x, y)
+        plt.title('Current open pulls per user')
+        plt.xlabel('Username')
+        plt.ylabel('Number of open pulls')
+        plt.show()
